941c16caa01d7c2c6bf69a3ea936ef1094009136
Baptiste <baptiste@opsmill.com>
2024-07-19 11:55:21 +0200

    Hotfix init to build docker

--- emma/__init__.py
+++ emma/__init__.py
@@ -1,5 +1,9 @@
 """Init file for Emma, mainly used to grab version from Poetry file."""
 
-import importlib.metadata
+# FIXME: For some reason importlib.metadata isn't install when building the docker
+# https://github.com/python-poetry/poetry/issues/2102
+# Thus the application is broken as it can find the package ...
+# import importlib.metadata
+# __version__ = importlib.metadata.version("emma")
 
-__version__ = importlib.metadata.version("emma")
+__version__ = "0.2.0"
