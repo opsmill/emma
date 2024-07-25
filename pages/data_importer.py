4f7d0f595f48a5a51099f5bcb79a8c313c44b487
wvandeun <wim@opsmill.com>
2024-07-25 23:18:49 +0200

    add dict_remove_nan_values to remove nan values from dataframes

--- pages/data_importer.py
+++ pages/data_importer.py
@@ -1,5 +1,6 @@
 import time
 from enum import Enum
+from typing import Any, Dict
 
 import pandas as pd
 import streamlit as st
@@ -25,6 +26,13 @@ class MessageSeverity(str, Enum):
 class Message(BaseModel):
     severity: MessageSeverity = MessageSeverity.INFO
     message: str
+
+
+def dict_remove_nan_values(data: Dict[str, Any]) -> Dict[str, Any]:
+    remove = [k for k,v in data.items() if pd.isnull(v)]
+    for k in remove:
+        data.pop(k)
+    return data
 
 
 def validate_if_df_is_compatible_with_schema(df: pd.DataFrame, target_schema: NodeSchema) -> list[Message]:
@@ -86,7 +94,11 @@ if option:
                 nbr_errors = 0
                 with st.status("Loading data...", expanded=True) as status:
                     for index, row in edited_df.iterrows():
-                        node = client.create(kind=option, **dict(row), branch=st.session_state.infrahub_branch)
+                        data = dict_remove_nan_values(dict(row))
+                        for key, value in data.items():
+                            if value == float("nan"):
+                                data.pop(key)
+                        node = client.create(kind=option, **data, branch=st.session_state.infrahub_branch)
                         node.save(allow_upsert=True)
                         edited_df.at[index, "Status"] = "ONGOING"
                         st.write(f"Item {index} CREATED id:{node.id}\n")
